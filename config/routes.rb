dbf687c780a4b838f2eeea86f3fc199e9119cacb
Brian Gueterman <bgueterm@uncc.edu>
2018-11-08 02:30:47 +0000

    massive changes to welcome/index and drinks/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
+  get 'drinks/index'
+  
+  resources :drinks
+  
   root 'welcome#index'
+  
+  root :to => 'drinks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
