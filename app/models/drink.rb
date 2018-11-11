ede5ee975391d902fb1e986d8d6741fdc0400a6d
Chris Fortenberry <cforten2@uncc.edu>
2018-11-11 01:46:49 +0000

    Modified flavor enum and added validation for models

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -5,4 +5,7 @@ class Drink < ApplicationRecord
     has_many :Mixer, :through => :DrinkMixer
     
     enum flavor: { froozen: 0, fruity: 1, hot: 2, savory: 3, sour: 4, spicy: 5, sweet: 6 }
+    
+    validates :name, presence: true, length: { minimum: 5 }
+    validates :alcoholPerVolume, presence: true, numericality: { greater_than_or_equal_to: 0.00, less_than_or_equal_to: 100.00}
 end
