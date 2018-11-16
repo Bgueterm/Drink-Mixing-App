971f50c3711aa50ec30a2ad3437bfc2bce4eaff7
Sarah <swelty2@uncc.edu>
2018-11-16 20:27:43 +0000

    Added CRUD functions in Drink controller

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,5 +1,12 @@
 class DrinksController < ApplicationController
     def index
+        @drinks = Drink.all
+    end
+    
+    def new
+        @drink = Drink.new
+      #  @drink.DrinkLiquor.build
+       # @drink.DrinkMixer.build
     end
     
     def search
@@ -7,13 +14,48 @@ class DrinksController < ApplicationController
     end    
 
     def show
+        @drink = Drink.find(params[:id])
     end
     
-   def create
+     def create
+        @drink = Drink.new(drink_params)
+
+        if @drink.save
+            redirect_to @drink
+        else
+            render 'new'
+        end
     end
     
     def results
         
     end
+    
+        def edit
+        @drink = Drink.find(params[:id])
+    end
+    
+    def update
+        @drink = Drink.find(params[:id])
+        
+        if @drink.update(drink_params)
+            redirect_to @drink
+        else
+            render 'edit'
+        end
+    end    
+    
+    def destroy
+        @drink = Drink.find(params[:id])
+        @drink.destroy
+        
+        redirect_to drinks_path
+    end
 
+    
+    private 
+        def drink_params
+            params.require(:drink).permit(:name, :alcoholPerVolume, :flavor,
+            {Liquor_ids: [], Mixer_ids: []})
+        end
 end
