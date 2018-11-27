678271754817359dbd6447bb448ec0d97728788f
Brian Gueterman <bgueterm@uncc.edu>
2018-11-27 16:12:05 +0000

    Made search page pretty

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,14 @@
 
-<<<<<<< HEAD
-Liquor.create([{ name: 'Rum' },
-                { name: 'Vodka' }])
-                
-Mixer.create([{ name: 'Mint leaves' },
-=======
 
 Liquor.create([{ name: 'Rum' },
                 { name: 'Vodka' }])
                 
 Mixer.create!([{ name: 'Mint leaves' },
->>>>>>> SarahBranch
               { name: 'Simple syrup' },
               { name: 'Lime juice' },
               { name: 'Club soda' },
               { name: 'Ginger beer' }])
               
-<<<<<<< HEAD
-Drink.create([{ name: 'Mojito', alcoholPerVolume: 15.5, flavor: :Sweet, 
-                description: "In a shaker, lightly muddle the mint.
-
-Add the rum, simple syrup and lime juice, fill with ice and strain.
-
-Pour into a highball glass.
-
-Top with the club soda.
-
-Garnish with a mint sprig.", liquor_ids: [1], mixer_ids: [1, 2, 3]},
-                { name: 'Moscow Mule', alcoholPerVolume: 25.5, flavor: :Sour,
-                  description: "Add all of the ingredients intoto a Moscow Mule mug (or highball glass) over ice.
-
-Garnish with a lime wheel.", liquor_ids: [2], mixer_ids: [3, 5]}])
-=======
 Drink.create!([{ name: 'Mojito', alcoholPerVolume: 15.5, flavor: :SWEET, 
                 description: "In a shaker, lightly muddle the mint.
  Add the rum, simple syrup and lime juice, fill with ice and strain.
@@ -43,4 +20,3 @@ Drink.create!([{ name: 'Mojito', alcoholPerVolume: 15.5, flavor: :SWEET,
  Garnish with a lime wheel.", liquor_ids: [2], mixer_ids: [3, 5]}]) 
  
  p "Created #{Drink.count} drinks"
->>>>>>> SarahBranch
